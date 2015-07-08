3efb6bfcf9617ff193e670e4aff8af1b01424817
ungentilgarçon <gregory.bahde@laposte.net>
2015-07-08 11:58:01 +0200

    polygons avec count > 5

--- Apps/CesiumTopo/testcreate_geojson_polygon_from_viz_json.py
+++ Apps/CesiumTopo/testcreate_geojson_polygon_from_viz_json.py
@@ -1,35 +1,6 @@
 # -- coding: utf-8 --
 # GENERATEUR DE FEATURE COLLECTION
-import json
-import os
-from geojson import Feature, Point, FeatureCollection, Polygon
-
-'''//ouvrir fichier points de viz, utiliser geojson pour en faire un geojson
-import json'''
-
-dataout_file = open('exportbands_all_venues.geojson','a+');
-f = open('exportbands_all_venues.json');
-
-data = json.loads(f.read())
-
-f.close()
-
-width = 4	
-z = len(data)
-print z
-for row in data:
-		print row
-		# TODO: pass if null
-		# pour l'instant
-		my_poly = Polygon([[(
-			row['longitude']-float(row['count'])/width,
-		 row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
-		  row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
-		   row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
-		    row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
-		     row['latitude']+float(row['count'])/width)]])
-
-# GENENRATEUR DE POLYGONES DEPUIS UN POINT GEO
+# DE POLYGONES DEPUIS UN POINT GEO
 # carré:
 # "({1}-{3},{2}+{3})({1}{3},{2}{3})({1}{3},{2}{3})({1}{3},{2}{3})({1}{3},{2}{3})"
 # soit:
@@ -61,22 +32,67 @@ for row in data:
 # i++ 
 # on peut aussi faire la moitie du travail et reintegrer {4}et {5} et se passer alors des nombres premiers...
 # 
+import json
+import os
+from geojson import Feature, Point, FeatureCollection, Polygon
+
+'''//ouvrir fichier points de viz, utiliser geojson pour en faire un geojson
+import json'''
+
+dataout_file = open('exportbands_all_venues.geojson','a+');
+f = open('exportbands_all_venues-1000.json');
+
+data = json.loads(f.read())
+
+f.close()
+
+width = 4	
+z = len(data)
+print z
+y = 0
+for row in data:
+	
+	print row
+	print row['count']
+	print z
+	if row['count'] > 5:
+		# TODO: pass if null
+		# pour l'instant
+		my_poly = Polygon([[(
+			row['longitude']-float(row['count'])/width,
+		 row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
+		  row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
+		   row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
+		    row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
+		     row['latitude']+float(row['count'])/width)]])
+
 
 		geojs = Feature(geometry=my_poly,properties={"type": "Salle"})		
 		# print my_poly
 		# print geojs
-		print z
-		if z == len(data): 
-		 	# geocoll = "{'type': 'FeatureCollection', 'features': ["
-			dataout_file.write("{\"type\": \"FeatureCollection\", \"features\": [")
-		 	json.dump(geojs,dataout_file)
-		 	z -= 1
-		elif z == 1:	
+		if z == 1:
+			json.dump(geojs,dataout_file)	
 			terminator = "]}"
 			dataout_file.write(terminator)
 			z -= 1		
+		elif y == 0: 
+		 	# geocoll = "{'type': 'FeatureCollection', 'features': ["
+			dataout_file.write("{\"type\": \"FeatureCollection\", \"features\": [")
+		 	json.dump(geojs,dataout_file)
+		 	y += 1
+		 	z -= 1
+		 	print y
 		else :
 			dataout_file.write(',')
 			json.dump(geojs,dataout_file)
 			z -= 1
+			# json.dump (geojsvirgule,dataout_file)
+	else: 
+			if z == 1:	
+			
+				terminator = "]}"
+				dataout_file.write(terminator)
+				z -= 1		
+			else :
+				z -= 1
 			# json.dump (geojsvirgule,dataout_file)

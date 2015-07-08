9c87655c4fb64067f871b5bdb489d26a7a31e6b7
ungentilgarçon <gregory.bahde@laposte.net>
2015-07-08 10:06:29 +0200

    ca marche mais trop de point pour cesium aussi

--- Apps/CesiumTopo/testcreate_geojson_polygon_from_viz_json.py
+++ Apps/CesiumTopo/testcreate_geojson_polygon_from_viz_json.py
@@ -1,4 +1,5 @@
 # -- coding: utf-8 --
+# GENERATEUR DE FEATURE COLLECTION
 import json
 import os
 from geojson import Feature, Point, FeatureCollection, Polygon
@@ -6,21 +7,27 @@ from geojson import Feature, Point, FeatureCollection, Polygon
 '''//ouvrir fichier points de viz, utiliser geojson pour en faire un geojson
 import json'''
 
-with open('exportbands_all_venues-1000.geojson','a+') as dataout_file:
-	f = open('exportbands_all_venues-1000.json');
+dataout_file = open('exportbands_all_venues.geojson','a+');
+f = open('exportbands_all_venues.json');
 
-	data = json.loads(f.read())
+data = json.loads(f.read())
 
-	f.close()
+f.close()
 
-	for row in data:
+width = 4	
+z = len(data)
+print z
+for row in data:
+		print row
+		# TODO: pass if null
+		# pour l'instant
 		my_poly = Polygon([[(
-			row['longitude']-float(row['count'])/1000,
-		 row['latitude']+float(row['count'])/1000),(row['longitude']-float(row['count'])/1000,
-		  row['latitude']-float(row['count'])/1000),(row['longitude']+float(row['count'])/1000,
-		   row['latitude']-float(row['count'])/1000),(row['longitude']+float(row['count'])/1000,
-		    row['latitude']+float(row['count'])/1000),(row['longitude']-float(row['count'])/1000,
-		     row['latitude']+float(row['count'])/1000)]])
+			row['longitude']-float(row['count'])/width,
+		 row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
+		  row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
+		   row['latitude']-float(row['count'])/width),(row['longitude']+float(row['count'])/width,
+		    row['latitude']+float(row['count'])/width),(row['longitude']-float(row['count'])/width,
+		     row['latitude']+float(row['count'])/width)]])
 
 # GENENRATEUR DE POLYGONES DEPUIS UN POINT GEO
 # carré:
@@ -37,13 +44,13 @@ with open('exportbands_all_venues-1000.geojson','a+') as dataout_file:
 # 01
 # __
 # 
-# 3,5, et plus:
-#au besoin:
+# 3 aretes et plus:
+#au besoin:*
 #({1}+{3}*cos{6},{2}+{3}*sin{6})n+1 fois
 # n étant le nombre d'arêtes
 # 7 etant égal à cos ou sin
 # et avec {4}{5}{6} décrivant:
-# {3} = float(row['count'])/1000
+# {3} = float(row['count'])/width
 # {6}=angle
 # u = unit
 # v = 360
@@ -52,16 +59,24 @@ with open('exportbands_all_venues-1000.geojson','a+') as dataout_file:
 # for i = 0 to n
 # {6}=i*pas
 # i++ 
-# on peut aussifaire la moitie du travail et reintegrer {4}et {5} et se passer alors des nombres premiers
+# on peut aussi faire la moitie du travail et reintegrer {4}et {5} et se passer alors des nombres premiers...
 # 
 
 		geojs = Feature(geometry=my_poly,properties={"type": "Salle"})		
-		print my_poly
-		print geojs
-		geocoll = FeatureCollection([geojs])
-
-		json.dump (geocoll,dataout_file)
-
-
-
-liste_de_polygones = dict()
+		# print my_poly
+		# print geojs
+		print z
+		if z == len(data): 
+		 	# geocoll = "{'type': 'FeatureCollection', 'features': ["
+			dataout_file.write("{\"type\": \"FeatureCollection\", \"features\": [")
+		 	json.dump(geojs,dataout_file)
+		 	z -= 1
+		elif z == 1:	
+			terminator = "]}"
+			dataout_file.write(terminator)
+			z -= 1		
+		else :
+			dataout_file.write(',')
+			json.dump(geojs,dataout_file)
+			z -= 1
+			# json.dump (geojsvirgule,dataout_file)
